827552d65dbd374f4179c66d606a29eadd498e87
Sajmir <sajmir.buzi@studio.unibo.it>
2025-08-19 16:46:20 +0200

    modified Deck.scala added fun standardShuffled

--- src/main/scala/it/unibo/bluff/model/Deck.scala
+++ src/main/scala/it/unibo/bluff/model/Deck.scala
@@ -8,8 +8,11 @@ trait Deck:
 object Deck:
   def standard(shuffler: Shuffler): Deck =
     ListDeck(shuffler.shuffle(Card.fullDeck))
+  def standardShuffled(seed : Long) : Deck =
+    standard(Shuffler.deterministic(seed))
 final case class ListDeck(private val cards: List[Card]) extends Deck:
   def draw(n: Int): (List[Card], Deck) =
     val (taken, rest) = cards.splitAt(n)
     (taken, ListDeck(rest))
   def size: Int = cards.size
+  override def toString(): String = s"Deck($size cards)"
